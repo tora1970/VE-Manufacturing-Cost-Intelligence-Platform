2d06beefa1429f49cc3d3a2a45e51aec6e13b633
tora1970 <tora@bang-olufsen.dk>
2026-09-18 12:03:32 +0200

    Update technology_engine.py

--- engine/technology_engine.py
+++ engine/technology_engine.py
@@ -1,1 +1,25 @@
+import pandas as pd
 
+
+class TechnologyEngine:
+
+    def __init__(self, rules_df):
+        self.rules = rules_df
+
+    def recommend(self, volume):
+
+        technology = "Unknown"
+
+        for _, rule in self.rules.iterrows():
+
+            if rule["Operator"] == "<":
+
+                if volume < rule["Value"\]:
+                    technology = rule["Recommended Technology"]
+
+            elif rule["Operator"] == ">=":
+
+                if volume >= rule["Value"\]:
+                    technology = rule["Recommended Technology"]
+
+        return technology
