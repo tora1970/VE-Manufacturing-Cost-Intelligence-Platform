7b003b906b5664aecd63798752652e68c3b6c045
tora1970 <tora@bang-olufsen.dk>
2026-09-18 12:24:38 +0200

    Fix comparison operator syntax in technology_engine.py

--- engine/technology_engine.py
+++ engine/technology_engine.py
@@ -11,12 +11,12 @@ class TechnologyEngine:
 
             if rule["Operator"] == "<":
 
-                if volume < rule["Value"]:
+                if volume < rule["Value"\]:
                     technology = rule["Recommended Technology"]
 
             elif rule["Operator"] == ">=":
 
-                if volume >= rule["Value"]:
+                if volume >= rule["Value"\]:
                     technology = rule["Recommended Technology"]
 
         return technology
