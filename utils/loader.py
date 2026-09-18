1dc3f71d2e20e6f3299958b0089d9a1cc74d0fdc
tora1970 <tora@bang-olufsen.dk>
2026-09-18 10:19:53 +0200

    Implement MasterDataLoader for loading Excel files

--- utils/loader.py
+++ utils/loader.py
@@ -1,1 +1,18 @@
+from pathlib import Path
+import pandas as pd
 
+
+class MasterDataLoader:
+
+    def __init__(self, masterdata_path="data/masterdata"):
+        self.masterdata_path = Path(masterdata_path)
+
+    def load_all(self):
+
+        data = {}
+
+        for file in self.masterdata_path.glob("*.xlsx"):
+            key = file.stem.lower()
+            data[key] = pd.read_excel(file)
+
+        return data
