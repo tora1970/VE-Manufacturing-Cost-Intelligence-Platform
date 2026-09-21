27e86b4e1e69f7446e33317fe3b1475c428771d1
tora1970 <tora@bang-olufsen.dk>
2026-09-21 15:28:06 +0200

    Refactor MasterDataLoader methods for readability
    Refactor the __init__ method and load_all method for better readability.

--- utils/loader.py
+++ utils/loader.py
@@ -4,15 +4,26 @@ import pandas as pd
 
 class MasterDataLoader:
 
-    def __init__(self, masterdata_path="data/masterdata"):
-        self.masterdata_path = Path(masterdata_path)
+    def __init__(
+        self,
+        masterdata_path="data/masterdata"
+    ):
+        self.masterdata_path = Path(
+            masterdata_path
+        )
 
     def load_all(self):
 
         data = {}
 
-        for file in self.masterdata_path.glob("*.xlsx"):
+        for file in self.masterdata_path.glob(
+            "*.xlsx"
+        ):
+
             key = file.stem.lower()
-            data[key] = pd.read_excel(file)
+
+            data[key] = pd.read_excel(
+                file
+            )
 
         return data
