6c91cf28097f10ef5b6973aa93ade7efecf983f7
tora1970 <tora@bang-olufsen.dk>
2026-09-22 13:29:26 +0200

    Remove baseline technology selection
    Removed selection of baseline technology from the app.

--- app.py
+++ app.py
@@ -302,19 +302,6 @@ if st.button("Recommend Technology"):
     st.subheader(
         "Technology Ranking"
     )
-available_technologies = (
-    benchmark_df[
-        "Technology"
-    ].tolist()
-)
-
-baseline_technology = (
-    st.selectbox(
-        "Baseline Technology",
-        available_technologies,
-        index=0
-    )
-)
     
     st.dataframe(
         benchmark_df,
